1d3d69c8e8e05769d48495c7b6892004215c6f05
Yashya <yashyakumarganji@gmail.com>
2025-02-05 10:16:06 +0530

    Revert "Changes airport sorting. Fixes AB#408"
    This reverts commit 307914db68b0a45d2874fad61af3efa8104fe4b3.

--- src/services/airports.service.js
+++ src/services/airports.service.js
@@ -8,8 +8,9 @@ class AirportsService {
     }
 
     getAll(){
-        return this._airports.filter(a => a.code).map(avoidEmptyCity).sort((a, b) => (a.city > b.city) ? 1 : -1);
+        return this._airports.filter(a => a.code).map(avoidEmptyCity);
     }
+
     getByCode(code) {
         return avoidEmptyCity(this._airports.find(a => a.code == code));
     }
